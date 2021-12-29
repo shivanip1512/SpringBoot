bd0e12522b0f37184b4644524e37ee1133e412bc
shivanip1512 <shivanipacharne10@gmail.com>
2021-12-30 00:56:24 +0530

    CRUD in SpringBoot JPA

--- SpringBootJPA/src/main/java/com/shivani/springboot/jpa/SpringBootJpaApplication.java
+++ SpringBootJPA/src/main/java/com/shivani/springboot/jpa/SpringBootJpaApplication.java
@@ -1,5 +1,9 @@
 package com.shivani.springboot.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,13 +17,40 @@ public class SpringBootJpaApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(SpringBootJpaApplication.class, args);
 		UserRepo userRepo = applicationContext.getBean(UserRepo.class);
+
+		/*
+		 * User u = new User(); u.setName("Shivani");
+		 * u.setStatus("I am a Java Programmer");
+		 * 
+		 * saving single object User user = userRepo.save(u); System.out.println(user);
+		 */
+
+		/*
+		 * User u1=new User(); u1.setName("name1"); u1.setStatus("python programmer");
+		 * 
+		 * 
+		 * User u2=new User(); u2.setName("name2"); u2.setStatus("c++ programmer");
+		 * 
+		 * List<User> list = new ArrayList<>(); list.add(u1); list.add(u2);
+		 * 
+		 * //save multiple objects Iterable<User> saveAll = userRepo.saveAll(list);
+		 * 
+		 * saveAll.forEach(u->System.out.println(u));
+		 */
+
+		/*
+		 * // update Optional<User> user = userRepo.findById(2);
+		 * 
+		 * User user2 = user.get(); user2.setName("Sweety");
+		 * user2.setStatus("C Programmer");
+		 * 
+		 * User updatedUser = userRepo.save(user2); System.out.println(updatedUser);
+		 */
 		
-		User u = new User();
-		u.setName("Shivani");
-		u.setStatus("I am a Java Programmer");
-		
-		User user = userRepo.save(u);
-		System.out.println(user);
+		/*
+		 * //delete 
+		 * userRepo.deleteById(3);
+		 */
 	}
 
 }
