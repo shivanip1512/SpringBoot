35779067373808bede4dfc14fc017ad918052589
shivanip1512 <shivanipacharne10@gmail.com>
2022-01-07 00:11:04 +0530

    dynamic path to target folder

--- SpringBoot-REST-API/src/main/java/com/shivani/springboot/restAPI/util/FileUploadHelper.java
+++ SpringBoot-REST-API/src/main/java/com/shivani/springboot/restAPI/util/FileUploadHelper.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.aspectj.apache.bcel.util.ClassPath;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,7 +15,7 @@ public class FileUploadHelper {
 
 	public String UPLOAD_DIR;
 
-	public boolean uploadFile(MultipartFile file) {
+	public boolean uploadFile(MultipartFile file, String dir) {
 		try {
 
 			/*
@@ -26,6 +28,7 @@ public class FileUploadHelper {
 			 * 
 			 * fileOutputStream.flush(); fileOutputStream.close(); f=true;
 			 */
+			UPLOAD_DIR = new ClassPathResource(dir).getFile().getAbsolutePath();
 
 			Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR + File.separator + file.getOriginalFilename()),
 					StandardCopyOption.REPLACE_EXISTING);
